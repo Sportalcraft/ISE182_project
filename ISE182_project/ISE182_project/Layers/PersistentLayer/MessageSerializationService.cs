94771d11bf10c1d34d49a0ea2fa7ede4e63505ad
Tal Barzilay <talbarzi@post.bgu.ac.il>
2018-04-03 20:05:38 +0300

    changed a bit the logger ad add few tries on the percictent layer

--- ISE182_project/ISE182_project/Layers/PersistentLayer/MessageSerializationService.cs
+++ ISE182_project/ISE182_project/Layers/PersistentLayer/MessageSerializationService.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ISE182_project.Layers.LoggingLayer;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,16 +17,23 @@ namespace ISE182_project.Layers.PersistentLayer
         // serialize a *sorted* list of messages
         public static void serialize(ArrayList messages)
         {
+            Logger.Log.Debug(Logger.MethodStart(MethodBase.GetCurrentMethod()));
+
             SerializationService.serialize(messages, MESSAGE_LIST);
         }
 
         //Deserialize all messages from the disk
         public static ArrayList deserialize()
         {
+            Logger.Log.Debug(Logger.MethodStart(MethodBase.GetCurrentMethod()));
+
             ArrayList temp = (ArrayList)SerializationService.deserialize(MESSAGE_LIST);
 
             if (temp == null)
+            {
+                Logger.Log.Warn(Logger.Developer("deserialized null messefes list from " + MESSAGE_LIST + ", returning an empty Arraylist"));
                 return new ArrayList();
+            }
 
             return temp;
         }
