dc14cbbce343fb420908a6a585a6de372b02a916
Tal Barzilay <talbarzi@post.bgu.ac.il>
2018-04-03 04:38:57 +0300

    add logger class

--- ISE182_project/ISE182_project/Program.cs
+++ ISE182_project/ISE182_project/Program.cs
@@ -1,19 +1,30 @@
 ﻿using ISE182_project.Layers.BusinessLogic;
 using ISE182_project.Layers.CommunicationLayer;
+using ISE182_project.Layers.LoggingLayer;
 using ISE182_project.Layers.PersistentLayer;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
+
+
 namespace MileStoneClient
 {
     class Program
-    {
+    { 
          static void Main(string[] args)
         {
+            //Exciptions on english
+            System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+
             //ChatRoom.register("Tal");
-            ChatRoom.login("Tal");
+            //ChatRoom.login("Tal");
+            Logger.Log.Debug("DEBUG log");
+            Logger.Log.Info("INFO log");
+            Logger.Log.Warn("WARN log");
+            Logger.Log.Error("ERROR log");
+            Logger.Log.Fatal("FATAL log");
 
             Console.ReadKey();
         }
